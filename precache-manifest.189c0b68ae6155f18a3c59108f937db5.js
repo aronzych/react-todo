47a448ba7647fb619a604fccef5c713e8b2c7585
Aron <aron.zych@yahoo.com>
2020-03-23 17:08:12 +0100

    Updates

---
+++ precache-manifest.189c0b68ae6155f18a3c59108f937db5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b26907621fb41079db67f8ee52cb2e3",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "ef68ec042ffdfe6fbd22",
+    "url": "/react-todo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "aa87c206aae2a2257b89",
+    "url": "/react-todo/static/css/main.a83f4baf.chunk.css"
+  },
+  {
+    "revision": "ef68ec042ffdfe6fbd22",
+    "url": "/react-todo/static/js/2.0600f265.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.0600f265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa87c206aae2a2257b89",
+    "url": "/react-todo/static/js/main.d5726c4f.chunk.js"
+  },
+  {
+    "revision": "cfcc09d4cc49456cf754",
+    "url": "/react-todo/static/js/runtime-main.8d588a53.js"
+  }
+]);
